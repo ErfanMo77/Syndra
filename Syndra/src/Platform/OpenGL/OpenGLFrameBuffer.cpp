cfd527f46fb59924168278bc4aba001579675a06
ErfanMo77 <43669965+ErfanMo98@users.noreply.github.com>
2021-01-14 14:58:35 +0330

    Fixed FrameBuffers.

--- Syndra/src/Platform/OpenGL/OpenGLFrameBuffer.cpp
+++ Syndra/src/Platform/OpenGL/OpenGLFrameBuffer.cpp
@@ -1,2 +1,77 @@
 #include "lpch.h"
-#include "Platform/OpenGLFrameBuffer.h"
+#include "Platform/OpenGL/OpenGLFrameBuffer.h"
+#include <glad/glad.h>
+
+namespace Syndra {
+
+	static const uint32_t s_MaxFramebufferSize = 8192;
+
+	OpenGLFrameBuffer::OpenGLFrameBuffer(const FramebufferSpecification& spec)
+		:m_Specification(spec)
+	{
+		Invalidate();
+	}
+
+	OpenGLFrameBuffer::~OpenGLFrameBuffer()
+	{
+		glDeleteFramebuffers(1, &m_RendererID);
+	}
+
+	void OpenGLFrameBuffer::Invalidate()
+	{
+		glCreateFramebuffers(1, &m_RendererID);
+
+		if (m_RendererID)
+		{
+			glDeleteFramebuffers(1, &m_RendererID);
+			glDeleteTextures(1, &m_ColorAttachment);
+			glDeleteTextures(1, &m_DepthAttachment);
+		}
+
+		glCreateFramebuffers(1, &m_RendererID);
+		glBindFramebuffer(GL_FRAMEBUFFER, m_RendererID);
+
+		glCreateTextures(GL_TEXTURE_2D, 1, &m_ColorAttachment);
+		glBindTexture(GL_TEXTURE_2D, m_ColorAttachment);
+		//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_Specification.Width, m_Specification.Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+		glTextureStorage2D(m_RendererID, 1, GL_RGBA8, m_Specification.Width, m_Specification.Height);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTextureSubImage2D(m_RendererID, 0, 0, 0, m_Specification.Width, m_Specification.Height, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_ColorAttachment, 0);
+
+		glCreateTextures(GL_TEXTURE_2D, 1, &m_DepthAttachment);
+		glBindTexture(GL_TEXTURE_2D, m_DepthAttachment);
+		glTexStorage2D(GL_TEXTURE_2D, 1, GL_DEPTH24_STENCIL8, m_Specification.Width, m_Specification.Height);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, m_DepthAttachment, 0);
+
+		SN_CORE_ASSERT(glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE, "Framebuffer is incomplete!");
+
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	}
+
+	void OpenGLFrameBuffer::Bind()
+	{
+		glBindFramebuffer(GL_FRAMEBUFFER, m_RendererID);
+		glViewport(0, 0, m_Specification.Width, m_Specification.Height);
+	}
+
+	void OpenGLFrameBuffer::Unbind()
+	{
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	}
+
+	void OpenGLFrameBuffer::Resize(uint32_t width, uint32_t height)
+	{
+		if (width == 0 || height == 0 || width > s_MaxFramebufferSize || height > s_MaxFramebufferSize)
+		{
+			SN_CORE_ERROR("Attempted to rezize framebuffer to {0}, {1}", width, height);
+			return;
+		}
+		m_Specification.Width = width;
+		m_Specification.Height = height;
+
+		Invalidate();
+	}
+
+}
--- Syndra/src/Platform/OpenGL/OpenGLFrameBuffer.h
+++ Syndra/src/Platform/OpenGL/OpenGLFrameBuffer.h
@@ -2,9 +2,28 @@
 #include "Engine/Renderer/FrameBuffer.h"
 
 namespace Syndra {
+
 	class OpenGLFrameBuffer : public FrameBuffer
 	{
+	public:
+		OpenGLFrameBuffer(const FramebufferSpecification& spec);
+		virtual ~OpenGLFrameBuffer();
 
+		virtual void Invalidate();
+
+		virtual void Bind() override;
+		virtual void Unbind() override;
+
+		virtual void Resize(uint32_t width, uint32_t height) override;
+
+		virtual uint32_t GetColorAttachmentRendererID() const override { return m_ColorAttachment; }
+
+		virtual const FramebufferSpecification& GetSpecification() const override { return m_Specification; }
+
+	private:
+		uint32_t m_RendererID;
+		uint32_t m_ColorAttachment = 0, m_DepthAttachment = 0;
+		FramebufferSpecification m_Specification;
 	};
 }
 
