0355b745bc585ebaad6414b2ecefadcbc5d5a685
ErfanMo77 <43669965+ErfanMo98@users.noreply.github.com>
2021-06-07 22:47:29 +0430

    Fixed FXAA.

--- Syndra-Editor/assets/shaders/FXAA.glsl
+++ Syndra-Editor/assets/shaders/FXAA.glsl
@@ -20,12 +20,20 @@
 layout(location = 0) in vec3 a_pos;
 layout(location = 1) in vec2 a_uv;
 
-layout(location = 0) out vec2 v_uv;
+layout(location = 0) out vec4 v_uv;
+
+layout(push_constant) uniform push{
+    float width;
+    float height;
+}pc;
 
 void main()
 {
     gl_Position = vec4(a_pos,1.0);
-    v_uv = a_uv;
+    v_uv.xy = a_uv;
+
+    vec2 rcpFrame = vec2(1/(pc.width),1/(pc.height));
+	v_uv.wz = a_uv - (rcpFrame * (0.5));
 }
 
 #type fragment
@@ -33,106 +41,66 @@ void main()
 
 layout(location = 0) out vec4 FragColor;
 
-layout(location = 0) in vec2 v_uv;
+layout(location = 0) in vec4 v_uv;
+
+layout(binding = 0) uniform sampler2D u_Texture;
 
 layout(push_constant) uniform push{
     float width;
     float height;
 }pc;
 
-layout(binding = 0) uniform sampler2D u_Texture;
-
-/*
-FXAA_PRESET - Choose compile-in knob preset 0-5.
-------------------------------------------------------------------------------
-FXAA_EDGE_THRESHOLD - The minimum amount of local contrast required 
-                      to apply algorithm.
-                      1.0/3.0  - too little
-                      1.0/4.0  - good start
-                      1.0/8.0  - applies to more edges
-                      1.0/16.0 - overkill
-------------------------------------------------------------------------------
-FXAA_EDGE_THRESHOLD_MIN - Trims the algorithm from processing darks.
-                          Perf optimization.
-                          1.0/32.0 - visible limit (smaller isn't visible)
-                          1.0/16.0 - good compromise
-                          1.0/12.0 - upper limit (seeing artifacts)
-------------------------------------------------------------------------------
-FXAA_SEARCH_STEPS - Maximum number of search steps for end of span.
-------------------------------------------------------------------------------
-FXAA_SEARCH_THRESHOLD - Controls when to stop searching.
-                        1.0/4.0 - seems to be the best quality wise
-------------------------------------------------------------------------------
-FXAA_SUBPIX_TRIM - Controls sub-pixel aliasing removal.
-                   1.0/2.0 - low removal
-                   1.0/3.0 - medium removal
-                   1.0/4.0 - default removal
-                   1.0/8.0 - high removal
-                   0.0 - complete removal
-------------------------------------------------------------------------------
-FXAA_SUBPIX_CAP - Insures fine detail is not completely removed.
-                  This is important for the transition of sub-pixel detail,
-                  like fences and wires.
-                  3.0/4.0 - default (medium amount of filtering)
-                  7.0/8.0 - high amount of filtering
-                  1.0 - no capping of sub-pixel aliasing removal
-*/
-
-const float FXAA_REDUCE_MIN = (1.0/ 128.0);
-const float FXAA_REDUCE_MUL = (1.0 / 8.0);
-const float FXAA_SPAN_MAX = 8.0;
-
-
-
-// Return the luma, the estimation of luminance from rgb inputs.
-// This approximates luma using one FMA instruction,
-// skipping normalization and tossing out blue.
-// FxaaLuma() will range 0.0 to 2.963210702.
-float FxaaLuma(vec3 rgb) {
-    return rgb.y * (0.587/0.299) + rgb.x;
-}
-
-vec3 FxaaLerp3(vec3 a, vec3 b, float amountOfA) {
-    return (vec3(-amountOfA) * b) + ((a * vec3(amountOfA)) + b);
-}
-
-vec4 FxaaTexOff(sampler2D tex, vec2 pos, ivec2 off, vec2 rcpFrame) {
-    float x = pos.x + float(off.x) * rcpFrame.x;
-    float y = pos.y + float(off.y) * rcpFrame.y;
-    return texture(tex, vec2(x, y));
-}
-
-// pos is the output of FxaaVertexShader interpolated across screen.
-// xy -> actual texture position {0.0 to 1.0}
-// rcpFrame should be a uniform equal to  {1.0/frameWidth, 1.0/frameHeight}
-vec4 FxaaPixelShader(vec2 pos, sampler2D tex, vec2 rcpFrame)
-{
-    vec3 rgbN = FxaaTexOff(tex, pos, ivec2( 0,-1), rcpFrame).xyz;
-    vec3 rgbW = FxaaTexOff(tex, pos, ivec2(-1, 0), rcpFrame).xyz;
-    vec3 rgbM = FxaaTexOff(tex, pos, ivec2( 0, 0), rcpFrame).xyz;
-    vec3 rgbE = FxaaTexOff(tex, pos, ivec2( 1, 0), rcpFrame).xyz;
-    vec3 rgbS = FxaaTexOff(tex, pos, ivec2( 0, 1), rcpFrame).xyz;
-    
-    float lumaN = FxaaLuma(rgbN);
-    float lumaW = FxaaLuma(rgbW);
-    float lumaM = FxaaLuma(rgbM);
-    float lumaE = FxaaLuma(rgbE);
-    float lumaS = FxaaLuma(rgbS);
-    
-    vec4 color;
-
-    vec2 v_rgbNW = (pos + vec2(-1.0, -1.0)) * rcpFrame;
-	vec2 v_rgbNE = (pos + vec2(1.0, -1.0)) * rcpFrame;
-	vec2 v_rgbSW = (pos + vec2(-1.0, 1.0)) * rcpFrame;
-	vec2 v_rgbSE = (pos + vec2(1.0, 1.0)) * rcpFrame;
-	vec2 v_rgbM = vec2(pos * rcpFrame);
-
-    return vec4(0);
-}
-
 void main()
 {
-   float sizeX = 1.0 / pc.width;
-   float sizeY = 1.0 / pc.height;
-   FragColor = FxaaPixelShader(v_uv, u_Texture, vec2(sizeX,sizeY));
+    vec2 resolution = vec2(pc.width, pc.height);
+    vec2 inverseVP = vec2(1.0 / resolution.x, 1.0 / resolution.y);
+
+    vec2 v_rgbNW = (v_uv.xy + vec2(-1.0, -1.0)) * inverseVP;
+	vec2 v_rgbNE = (v_uv.xy + vec2(1.0, -1.0)) * inverseVP;
+	vec2 v_rgbSW = (v_uv.xy + vec2(-1.0, 1.0)) * inverseVP;
+	vec2 v_rgbSE = (v_uv.xy + vec2(1.0, 1.0)) * inverseVP;
+	vec2 v_rgbM = vec2(v_uv.xy * inverseVP);
+
+    vec3 rgbNW = textureOffset(u_Texture, v_uv.xy,ivec2(-1,-1)).xyz;
+    vec3 rgbNE = textureOffset(u_Texture, v_uv.xy,ivec2(1,-1)).xyz;
+    vec3 rgbSW = textureOffset(u_Texture, v_uv.xy,ivec2(-1,1)).xyz;
+    vec3 rgbSE = textureOffset(u_Texture, v_uv.xy,ivec2(1,1)).xyz;
+    vec3 rgbM  = texture(u_Texture, v_rgbM).xyz;
+
+    vec3  luma = vec3(0.299, 0.587, 0.114);
+    float lumaNW = dot(rgbNW, luma);
+    float lumaNE = dot(rgbNE, luma);
+    float lumaSW = dot(rgbSW, luma);
+    float lumaSE = dot(rgbSE, luma);
+    float lumaM  = dot(rgbM,  luma);
+    float lumaMin = min(lumaM, min(min(lumaNW, lumaNE), min(lumaSW, lumaSE)));
+    float lumaMax = max(lumaM, max(max(lumaNW, lumaNE), max(lumaSW, lumaSE)));
+
+    vec2 dir;
+    dir.x = -((lumaNW + lumaNE) - (lumaSW + lumaSE));
+    dir.y =  ((lumaNW + lumaSW) - (lumaNE + lumaSE));
+    
+    float dirReduce = max((lumaNW + lumaNE + lumaSW + lumaSE) *
+                          (0.25 * (1.0 / 8.0)), (1.0/ 128.0));
+
+    float rcpDirMin = 1.0 / (min(abs(dir.x), abs(dir.y)) + dirReduce);
+    dir = min(vec2(8.0, 8.0),
+              max(vec2(-8.0, -8.0),
+              dir * rcpDirMin)) * inverseVP;
+
+    vec3 rgbA = 0.5 * (
+        texture(u_Texture, v_uv.xy + dir * (1.0 / 3.0 - 0.5)).xyz +
+        texture(u_Texture, v_uv.xy + dir * (2.0 / 3.0 - 0.5)).xyz);
+    vec3 rgbB = rgbA * 0.5 + 0.25 * (
+        texture(u_Texture, v_uv.xy + dir * -0.5).xyz +
+        texture(u_Texture, v_uv.xy + dir * 0.5).xyz);
+
+    float lumaB = dot(rgbB, luma);
+    vec4 color;
+    if ((lumaB < lumaMin) || (lumaB > lumaMax))
+        color = vec4(rgbA, 1.0);
+    else
+        color = vec4(rgbB, 1.0);
+
+    FragColor = color;
 } 
