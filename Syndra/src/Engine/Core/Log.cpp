312f89cb538da24a51aae989b96d7a1d0fdc17e2
ErfanMo77 <43669965+ErfanMo98@users.noreply.github.com>
2021-12-31 20:47:12 +0330

    Updated log configs.

--- Syndra/src/Engine/Core/Log.cpp
+++ Syndra/src/Engine/Core/Log.cpp
@@ -1,9 +1,10 @@
 #include "lpch.h"
 #include "Engine/Core/Log.h"
 #include "spdlog/spdlog.h"
-#include "spdlog/sinks/stdout_color_sinks.h"
-#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/stdout_sinks.h"
+#include "spdlog/sinks/base_sink.h"
 #include "spdlog/fmt/ostr.h"
+#include "spdlog/sinks/ostream_sink.h"
 
 namespace Syndra {
 
@@ -13,20 +14,19 @@ namespace Syndra {
 	void Log::init() {
 
 		std::vector<spdlog::sink_ptr> logSinks;
-		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Syndra-Debug.log", true));
-
-		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
-		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::wincolor_stderr_sink_mt>());
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::simple_file_sink_mt>("Syndra-Debug.log", true));
 
 		s_CoreLogger = std::make_shared<spdlog::logger>("ENGINE", begin(logSinks), end(logSinks));
 		spdlog::register_logger(s_CoreLogger);
 		s_CoreLogger->set_level(spdlog::level::trace);
+		s_CoreLogger->set_pattern("%^[%T] %n: %v%$");
 		s_CoreLogger->flush_on(spdlog::level::trace);
 
 		s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
 		spdlog::register_logger(s_ClientLogger);
 		s_ClientLogger->set_level(spdlog::level::trace);
+		s_ClientLogger->set_pattern("[%T] [%l] %n: %v");
 		s_ClientLogger->flush_on(spdlog::level::trace);
 
 	}
